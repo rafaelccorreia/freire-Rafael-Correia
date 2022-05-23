86f242ca1fd4adcb35925fe097a9d29a8f2c859d
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-05-23 14:59:39 -0300

    revisaoJS - Exercício 12

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -126,7 +126,8 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    pessoa.nome = 'ANÔNIMO'
+    return pessoa
 }
 
 // EXERCÍCIO 13A
