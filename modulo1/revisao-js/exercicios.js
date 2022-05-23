e2d99b7c4b630fac285c4035568ccf35f74e9321
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-05-23 14:26:17 -0300

    revisaoJS - Exercício 9

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -97,7 +97,15 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA === ladoB && ladoA === ladoC){
+        return "Equilátero"
+    }
+    else if(ladoA !== ladoB && ladoA !== ladoC && ladoB !== ladoC){
+        return "Escaleno"
+    }
+    else {
+        return "Isósceles"
+    }
 }
 
 // EXERCÍCIO 10
