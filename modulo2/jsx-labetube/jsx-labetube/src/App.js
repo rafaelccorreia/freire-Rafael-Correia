c0412b8341833296f0df80ba1c952e2b135902bb
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-06-06 22:35:50 -0300

    Adicionando últimas alterações do desafio

--- modulo2/jsx-labetube/jsx-labetube/src/App.js
+++ modulo2/jsx-labetube/jsx-labetube/src/App.js
@@ -6,6 +6,10 @@ import exploreIcon from './img/explore_icon.svg';
 import libraryIcon from './img/library_icon.svg';
 import historyIcon from './img/history_icon.svg';
 import tubeIcon from './img/tube_icon.svg';
+import searchIcon from './img/search_icon.svg';
+import micIcon from './img/mic_icon.svg';
+import dotsIcon from './img/vertical_dots_icon.svg';
+import dotsIcon2 from './img/nine_dots_icon.svg';
 
 function App() {
 
@@ -18,10 +22,21 @@ function App() {
   return (
     <div>
       <div className="tela-inteira">
+
         <header>
-            <h1>Lab <img src={tubeIcon} alt='ìcone Lab Tube' /> Tube</h1>
-            <input type="search" placeholder="Buscar" id="campoDeBusca"/>
+          <h1 className='logo-header'>Lab <img src={tubeIcon} alt='ìcone Lab Tube' /> Tube</h1>
+          <div className="campoDeBusca">
+            <input type="search" placeholder="Buscar" />
+            <div className='img-container'>
+              <img src={searchIcon} alt='ícone de lupa' />
+            </div>
+            <img src={micIcon} alt='ícone do microfone' className='mic-header' />
+          </div>
+          <div className='login-container'>
+            <img src={dotsIcon2} alt='' />
+            <img src={dotsIcon} alt='' />
             <button className='botao-header'>Fazer login</button>
+          </div>
         </header>
             
         <nav className="menu-vertical">
@@ -45,6 +60,18 @@ function App() {
               <img src={historyIcon} alt='ícone do histórico' />
               <p>Histórico</p>
             </li>
+          </ul>
+        </nav>
+
+        <nav className="menu-horizontal">
+          <ul className='lista-menu-horizontal'>
+            <li>Tudo</li>
+            <li>Música</li>
+            <li>Jogos</li>
+            <li>Comédia</li>
+            <li>Minecraft</li>
+            <li>Aulas</li>
+            <li>Programação</li>
           </ul>
         </nav>
 
@@ -81,6 +108,22 @@ function App() {
                 </div>
                 <div className="box-pagina-principal media8" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=8 " alt="" />
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media9" onClick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=9 " alt="" />
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media10" onClick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=10 " alt="" />
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media11" onClick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=11 " alt="" />
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media12" onClick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=12 " alt="" />
                     <h4>{titulo}</h4>
                 </div>
             </section>
