549aa5e41e9d32d6eda3f2ec361f8290a18a316c
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-07-05 16:57:59 -0300

    Criei um componente funcional

--- modulo3/projeto-astromatch/astromatch/src/App.js
+++ modulo3/projeto-astromatch/astromatch/src/App.js
@@ -1,10 +1,20 @@
 import './App.css'
+import styled from 'styled-components'
+import BigCard from './components/BigCard'
+
+const ContainerGeral = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+`
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <ContainerGeral className="App">
+      <BigCard />
+    </ContainerGeral>
   )
 }
 
---
+++ modulo3/projeto-astromatch/astromatch/src/components/BigCard.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+
+const BigCard = () => {
+    return (
+        <p>teste</p>
+    )
+}
+
+export default BigCard
