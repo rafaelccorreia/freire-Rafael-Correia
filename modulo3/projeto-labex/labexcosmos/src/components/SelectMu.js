b0619a371c47e6b8e8d089dadf911381a56435cb
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-07-15 19:53:48 -0300

    Finalizando a página de inscrição

--- modulo3/projeto-labex/labexcosmos/src/components/SelectMu.js
+++ modulo3/projeto-labex/labexcosmos/src/components/SelectMu.js
@@ -4,9 +4,11 @@ import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import { ThemeProvider } from '@mui/material/styles'
+import axios from 'axios'
+import { bgcolor, maxWidth } from '@mui/system'
 
 import theme from '../theme/theme'
-import { bgcolor, maxWidth } from '@mui/system'
+import { ListaDepaises } from '../constants/listaDePaises'
 
 export const SelectPlanet = (props) => {
     return (
@@ -42,6 +44,102 @@ export const SelectPlanet = (props) => {
                     <MenuItem value={'urano'}>Urano</MenuItem>
                     <MenuItem value={'netuno'}>Netuno</MenuItem>
                     <MenuItem value={'plutao'}>Plutão</MenuItem>
+                </Select>
+            </FormControl>
+        </ThemeProvider>
+    )
+}
+
+export const SelectTrip = (props) => {
+    const [listaViagens, setListaViagens] = React.useState([])
+    //requisição com a API
+    const GetTrips = () => {
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/rafael-correia-freire/trips')
+        .then(resp => {
+            setListaViagens(resp.data.trips)
+        })
+        .catch(err => {
+            alert(JSON.parse(err.request.responseText).message)
+        })
+    }
+
+    React.useEffect(() => {
+        GetTrips()
+    }, [])
+
+    const selectOptions = listaViagens.map(viagem => {
+        return (
+            <MenuItem value={viagem.name} key={viagem.id} id={viagem.id}>{viagem.name}</MenuItem>
+        )
+    })
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <FormControl 
+                variant="filled" 
+                fullWidth
+                sx={{ 
+                    label: {color: 'primary.main', fontWeight: 'bold'}, 
+                    input: {color: 'primary.main'},
+                    bgcolor: 'neutral.main',
+                    borderRadius: '10px',
+                    margin: '1rem 0 0.5rem 0',
+                    paddig: '1rem 0'
+                }}
+            >
+                <InputLabel id="select-trip-label">Escolha uma Viagem</InputLabel>
+                <Select
+                    labelId="select-trip-label"
+                    id="select-trip-label"
+                    value={props.value}
+                    onChange={props.onChange}
+                >
+                    <MenuItem value="">
+                        <em>Escolha uma Viagem</em>
+                    </MenuItem>
+                    {selectOptions}
+                </Select>
+            </FormControl>
+        </ThemeProvider>
+    )
+}
+
+export const SelectCountry = (props) => {
+    const listaPaises = ListaDepaises
+
+    const selectOptionsPais = listaPaises.map(pais => {
+        return (
+            <MenuItem value={pais.nome} key={pais.sigla3}>{pais.nome}</MenuItem>
+        )
+    })
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <FormControl 
+                variant="filled" 
+                fullWidth
+                sx={{ 
+                    label: {color: 'primary.main', fontWeight: 'bold'}, 
+                    input: {color: 'primary.main'},
+                    bgcolor: 'neutral.main',
+                    borderRadius: '10px',
+                    margin: '1rem 0 0.5rem 0',
+                    paddig: '1rem 0'
+                }}
+            >
+                <InputLabel id="select-country-label">Escolha um País</InputLabel>
+                <Select
+                    labelId="select-country-label"
+                    id="select-country-label"
+                    value={props.value}
+                    onChange={props.onChange}
+                >
+                    <MenuItem value="">
+                        <em>Escolha um País</em>
+                    </MenuItem>
+                    {selectOptionsPais}
                 </Select>
             </FormControl>
         </ThemeProvider>
--- modulo3/projeto-labex/labexcosmos/src/pages/ApplicationFormPage.js
+++ modulo3/projeto-labex/labexcosmos/src/pages/ApplicationFormPage.js
@@ -1,17 +1,176 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
 import BotaoVoltar from '../components/BotaoVoltar'
+import BotaoEscuro from '../components/BotaoEscuro'
+import MainContainer from '../components/MainContainer'
+import TituloDaTela from '../components/TituloDaTela'
+import { SelectTrip, SelectCountry } from '../components/SelectMu'
+import InputMu from '../components/InputMu'
+
+const FormPublicContainer = styled.div` 
+    background-color: rgba(185, 188, 191, 0.8);
+    min-width: 75%;
+    min-height: 80vh;
+    border-radius: 10px;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const FormElementoContainer = styled.form` 
+    min-width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #122b43;
+    border-radius: 10px;
+    padding: 1rem;
+`
+
+const InputsContainer = styled.div`
+    margin: 1rem;
+    min-width: 100%;
+    max-width: 800px;
+`
+
+const BotoesContainer = styled.div` 
+    display: flex;
+    justify-content: space-around;
+    min-width: 60%;
+    margin: 1rem;
+`
 
 const ApplicationFormPage = () => {
-    const enviarFormulario = () => {
-        //requisição aqui
+    const [valorSelectViagem, setValorSelectViagem] = useState('')
+    const [valorSelectViagemId, setValorSelectViagemId] = useState('')
+    const [valorSelectCountry, setValorSelectCountry] = useState('')
+    const [valorInputNome, setValorInputNome] = useState('')
+    const [valorInputIdade, setValorInputIdade] = useState('')
+    const [valorInputTexto, setValorInputTexto] = useState('')
+    const [valorInputProfissao, setValorInputProfissao] = useState('')
+
+    //controladores de input
+    const handleSelectViagem = (event) => {
+        setValorSelectViagem(event.target.value)
+        setValorSelectViagemId(event.target.id)
+    }
+    const handleSelectCountry = (event) => {
+        setValorSelectCountry(event.target.value)
+    }
+    const handleInputNome = (event) => {
+        setValorInputNome(event.target.value)
+    }
+    const handleInputIdade = (event) => {
+        setValorInputIdade(event.target.value)
+    }
+    const handleInputTexto = (event) => {
+        setValorInputTexto(event.target.value)
+    }
+    const handleInputProfissao = (event) => {
+        setValorInputProfissao(event.target.value)
+    }
+
+    const ApplyToTrip = () => {
+        const body = {
+            name: valorInputNome,
+            age: valorInputIdade,
+            applicationText: valorInputTexto,
+            profession: valorInputProfissao,
+            country: valorSelectCountry
+        }
+
+        axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/rafael-correia-freire/trips/${valorSelectViagemId}/apply`, body)
+        .then(resp => {
+            console.log(resp)
+            alert('Sua inscrição foi feita com sucesso!')
+            setValorInputIdade('')
+            setValorInputNome('')
+            setValorInputProfissao('')
+            setValorInputTexto('')
+            setValorSelectCountry('')
+            setValorSelectViagem('')
+            setValorSelectViagemId('')
+        })
+        .catch(err => {
+            alert(JSON.parse(err.request.responseText).message)
+        })
     }
 
     return (
-        <div>
-            Formulário de inscrição para viagem
-            <BotaoVoltar />
-            <button onClick={enviarFormulario}>Enviar</button>
-        </div>
+        <MainContainer>
+            <FormPublicContainer>
+                <TituloDaTela
+                    titulo={'Formulário de Inscrição'}
+                    color={'#121243'}
+                />
+                <FormElementoContainer>
+                    <InputsContainer>
+                        <SelectTrip 
+                            value={valorSelectViagem}
+                            onChange={handleSelectViagem}
+                        />
+                        <InputMu 
+                            backColor={'#b9bcbf'}
+                            color={'primary'}
+                            fontColor={'#122b43'}
+                            label={'Nome'}
+                            id={'NomeViagemAplicar'}
+                            onChange={handleInputNome}
+                            type={'text'}
+                            value={valorInputNome}
+                        />
+                        <InputMu 
+                            backColor={'#b9bcbf'}
+                            color={'primary'}
+                            fontColor={'#122b43'}
+                            label={'Idade'}
+                            id={'NomeViagemAplicar'}
+                            onChange={handleInputIdade}
+                            type={'number'}
+                            value={valorInputIdade}
+                        />
+                        <InputMu 
+                            backColor={'#b9bcbf'}
+                            color={'primary'}
+                            fontColor={'#122b43'}
+                            label={'Texto de candidatura'}
+                            id={'NomeViagemAplicar'}
+                            onChange={handleInputTexto}
+                            type={'text'}
+                            value={valorInputTexto}
+                        />
+                        <InputMu 
+                            backColor={'#b9bcbf'}
+                            color={'primary'}
+                            fontColor={'#122b43'}
+                            label={'Profissão'}
+                            id={'NomeViagemAplicar'}
+                            onChange={handleInputProfissao}
+                            type={'text'}
+                            value={valorInputProfissao}
+                        />
+                        <SelectCountry 
+                            value={valorSelectCountry}
+                            onChange={handleSelectCountry}
+                        />
+                    </InputsContainer>
+                </FormElementoContainer>
+                <BotoesContainer>
+                    <BotaoVoltar 
+                        color={'primary'}
+                    />
+                    <BotaoEscuro
+                        onClick={ApplyToTrip}
+                        texto={'Enviar'}
+                        color={'primary'}
+                    />
+                </BotoesContainer>
+            </FormPublicContainer>
+        </MainContainer>
     )
 }
 
