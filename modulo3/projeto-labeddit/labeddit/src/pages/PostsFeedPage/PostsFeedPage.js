4c03d9b6cbc89f08b234996f358426864a245f14
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-07-28 23:23:20 -0300

    Adicionei o formulário à página de feed

--- modulo3/projeto-labeddit/labeddit/src/pages/PostsFeedPage/PostsFeedPage.js
+++ modulo3/projeto-labeddit/labeddit/src/pages/PostsFeedPage/PostsFeedPage.js
@@ -2,15 +2,44 @@ import React from 'react'
 
 import useTela from '../../hooks/useTela'
 import useProtectedPage from '../../hooks/useProtectedPage'
+import useForm from '../../hooks/useForm'
+import { SquaredButtonFilled } from '../../components/BotaoGradient/BotaoGradient'
+import { MainContainer, InputStyled, TextAreaStyled, LinhaGradient } from './styled'
 
 const PostsFeedPage = () => {
     useProtectedPage()
+    const [dados, onChange, clear] = useForm({title: '', body: ''})
     useTela('Posts Feed Page')
 
+    const handleCriarPost = () => {
+
+    }
+
     return(
-        <div>
-            PostsFeed Page
-        </div>
+        <MainContainer>
+            <form onSubmit={handleCriarPost}>
+                <InputStyled
+                    name='title'
+                    value={dados.title}
+                    onChange={onChange}
+                    type={'text'}
+                    placeholder={'Título do post'}
+                />
+                <TextAreaStyled
+                    name='body'
+                    value={dados.body}
+                    onChange={onChange}
+                    type={'text'}
+                    placeholder={'Escreva seu post...'}
+                />
+                <SquaredButtonFilled 
+                    type={'submit'}
+                    text={'Postar'}
+                    value={'submit'}
+                />
+            </form>
+            <LinhaGradient />
+        </MainContainer>
     )
 }
 
