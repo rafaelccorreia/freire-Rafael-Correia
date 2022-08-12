a1b23b3d7ef04ae70889f13ba2492f1b552f8c10
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-08-12 00:01:21 -0300

    Exercício 7

--- modulo4/aprofundamento-typescript/src/exercicio7/exercicio7.ts
+++ modulo4/aprofundamento-typescript/src/exercicio7/exercicio7.ts
@@ -3,12 +3,38 @@ type Produto = {
     preco: number,
     classificacao: string
 }
+type Produto2 = {
+    nome: string,
+    preco: number,
+    classificacao: string,
+    precoDesconto: number
+}
+
+
 
 function calcularDesconto(listaProdutos: Produto[]): Produto[] {
-    const novaLista: Produto[] = listaProdutos.map(produto => {
+    let precoComDesconto: number
+    let newProduct:Produto2
+    const novaLista: Produto2[] = listaProdutos.map(produto => {
         switch(produto.classificacao) {
-            
+            case 'verao':
+                precoComDesconto = produto.preco * 0.05
+                newProduct = {...produto, precoDesconto: precoComDesconto}
+                break;
+            case 'inverno':
+                precoComDesconto = produto.preco * 0.1
+                newProduct = {...produto, precoDesconto: precoComDesconto}
+                break;
+            case 'banho':
+                precoComDesconto = produto.preco * 0.04
+                newProduct = {...produto, precoDesconto: precoComDesconto}
+                break;
+            case 'intimas':
+                precoComDesconto = produto.preco * 0.07
+                newProduct = {...produto, precoDesconto: precoComDesconto}
+                break;
         }
+        return newProduct
     })
     return novaLista
 }
