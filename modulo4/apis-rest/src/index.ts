861b4185ddc45e70b2ca6745367275f3cf8a47b4
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-08-18 15:44:10 -0300

    Exercício 3

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -11,28 +11,50 @@ app.use(cors());
 // Exercicio 1
 // a) R: Método Get
 // b) R: "/users" como entidade
+
 // Exercicio 2
 // a) R: Passei os parâmetros por query, escolhi por ser mais adequado à uma informação pequena e condicional que não é obrigatória
 // b) R: Coloquei uma condicional no código para enviar um aviso caso os parâmetros errados sejam passados
+
+//Exercicio 3
+// a) R: query
+// b) R: feito
 app.get('/users', (req,res) => {
     try {
         const userType: string | undefined = req.query.type as string
+        const userName: string | undefined = req.query.search as string
+
+        let listaFiltradaNome: User[]
+        if(!userName) {
+            listaFiltradaNome = users
+        } else {
+            listaFiltradaNome = users.filter(user => {
+                return user.name.toLowerCase().includes(userName.toLowerCase())
+            })
+            if(listaFiltradaNome.length < 1) {
+                throw new Error('No user found')
+            }
+        }
 
         if(!userType) {
-            res.status(200).send(users)
+            res.status(200).send(listaFiltradaNome)
         } else {
             if(userType === "NORMAL" || userType === "ADMIN") {
-                let newUsersList: User[] = users.filter(user => {
+                let newUsersList: User[] = listaFiltradaNome.filter(user => {
                     return user.type === userType
                 })
                 res.status(200).send(newUsersList)
             } else {
                 console.warn('Invalid query! ADMIN or NORMAL expected.')
-                res.send(users)
+                res.send(listaFiltradaNome)
             }
         }
     } catch (error: any) {
-        res.status(500).send({message: "Unexpected Error"})
+        if(error.message === 'No user found') {
+            res.status(422).send({message: error.message})
+        } else {
+            res.status(500).send({message: "Unexpected Error"})
+        }
     }
 })
 
