3bd7bf3037a09ff73447ac4c6c3e4b49d64690fa
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-08-18 14:34:57 -0300

    Exercício 1

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express } from "express";
 import cors from "cors";
 
+import { users } from "./data/users";
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
+
+// Exercicio 1
+// a) R: Método Get
+// b) R: "/users" como entidade
+app.get('/users', (req,res) => {
+    res.status(200).send(users)
+})
 
 const server = app.listen(3003, () => {
     console.log(`Server is running in http://localhost:3003`);
