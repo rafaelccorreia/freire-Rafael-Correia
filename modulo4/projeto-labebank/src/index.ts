913b30dba98a99a82467ae401e22b90baa0a9d30
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-08-19 17:52:46 -0300

    Requisição para fazer transferencia interna

--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -284,6 +284,101 @@ app.put('/contas/pagar', (req,res) => {
     }
 })
 
+app.put('/contas/transferencia', (req,res) => {
+    let errorCode: number = 400
+    try {
+        const { nome, CPF, nomeRecebe, CPFRecebe, valor} = req.body
+
+        if (!nome || !(typeof nome === "string")) {
+            errorCode = 422
+            throw new Error('Nome vazio ou inválido')
+        }
+        if (!CPF || !(typeof CPF === "string")) {
+            errorCode = 422
+            throw new Error('CPF vazio ou inválido')
+        }
+        if (!nomeRecebe || !(typeof nomeRecebe === "string")) {
+            errorCode = 422
+            throw new Error('Nome do recebedor vazio ou inválido')
+        }
+        if (!CPFRecebe || !(typeof CPFRecebe === "string")) {
+            errorCode = 422
+            throw new Error('CPF do recebedor vazio ou inválido')
+        }
+        if (!valor || !(typeof valor === "number") || valor <= 0) {
+            errorCode = 422
+            throw new Error('Valor da transferência inválido')
+        }
+
+        let valida1: boolean = false
+        let valida2: boolean = false
+        let novaListaContas: Conta[] = contas.map(conta => {
+            if(nome === conta.nome && CPF === conta.CPF){
+                valida1 = true
+                if(conta.saldo < valor) {
+                    errorCode = 422
+                    throw new Error('Saldo insuficiente')
+                } else {
+                    conta.saldo -= valor
+                }
+                return conta
+            } else {
+                return conta
+            }
+        })
+        let novaListaContasNovoSaldo: Conta[] = novaListaContas.map(conta => {
+            if(nomeRecebe === conta.nome && CPFRecebe === conta.CPF){
+                valida2 = true
+                conta.saldo += valor
+                return conta
+            } else {
+                return conta
+            }
+        })
+
+        if(!valida1) {
+            errorCode = 422
+            throw new Error('Nenhum usuário encontrado com essa combinação, verificar nome e CPF')
+        }
+        else if(!valida2) {
+            errorCode = 422
+            throw new Error('Nenhum recebedor encontrado com essa combinação, verificar nomeRecebedor e CPFRecebedor')
+        }
+        res.status(200).send({
+            message: "Tranferência executada com sucesso!",
+            novaListaContasNovoSaldo
+        })
+    }
+    catch(error: any) {
+        switch(error.message) {
+            case 'Nome vazio ou inválido':
+                res.status(errorCode).send({ message: error.message })
+                break
+            case 'CPF vazio ou inválido':
+                res.status(errorCode).send({ message: error.message })
+                break
+            case 'Nome do recebedor vazio ou inválido':
+                res.status(errorCode).send({ message: error.message })
+                break
+            case 'CPF do recebedor vazio ou inválido':
+                res.status(errorCode).send({ message: error.message })
+                break
+            case 'Valor da transferência inválido':
+                res.status(errorCode).send({ message: error.message })
+                break
+            case 'Nenhum usuário encontrado com essa combinação, verificar nome e CPF':
+                res.status(errorCode).send({ message: error.message })
+                break
+            case 'Nenhum recebedor encontrado com essa combinação, verificar nomeRecebedor e CPFRecebedor':
+                res.status(errorCode).send({ message: error.message })
+                break
+            default:
+                res.status(500).send({ message: 'Ocorreu um erro inesperado!'})
+                break
+        }
+    }
+})
+
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
         const address = server.address() as AddressInfo;
