1f3b27b1973f42be9de9b9b26148953f29b25e81
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-08-25 21:46:57 -0300

    Atualizando request de pegar user

--- modulo4/projeto-toDoList/src/data/selectUser.ts
+++ modulo4/projeto-toDoList/src/data/selectUser.ts
@@ -4,6 +4,10 @@ export default async function selectUser(id: string){
     const result = await connection.raw(`
         SELECT * FROM UserTodoList WHERE id = "${id}"
     `)
+    let resp: Object = {
+        "id": result[0][0].id,
+        "nickname": result[0][0].nickname
+    }
 
-    return result[0][0]
+    return JSON.stringify(resp)
 }
