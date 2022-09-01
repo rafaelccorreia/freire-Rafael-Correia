295c3892945c5402a48c989a2f30e3c552198147
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-09-01 11:10:16 -0300

    Estrutura incial do projeto

---
+++ modulo4/projeto-labecommerce/src/index.ts
@@ -0,0 +1,13 @@
+import { AddressInfo } from "net"
+import { app } from "./app"
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo
+        console.log(`Server is running in http://localhost: ${address.port}`)
+    } else {
+        console.error(`Failure upon starting server.`)
+    }
+})
