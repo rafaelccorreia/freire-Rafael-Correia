b5c02a244743a7a039b173272aba05898207bfc2
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-09-06 18:37:34 -0300

    Exercícios 1, 2 e 3 Polimorfismo

--- modulo5/heranca-polimorfismo/src/index.ts
+++ modulo5/heranca-polimorfismo/src/index.ts
@@ -1,3 +1,5 @@
+/////////////////// HERANÇA
+
 class User {
     private id: string;
     private email: string;
@@ -79,5 +81,105 @@ console.log(customer.getId(), customer.getName(), customer.getEmail(), customer.
     a) A senha aparece no console, porém não seria possível já que a senha está atrelada a classe User, e não foi criado nenhum método public para acessá-la
 */
 
-// Exercício 4
+// Exercício 4 e 5
 console.log(customer.introduceYourserlf())
+
+//////////////////////// POLIMORFISMO
+
+export interface Client {
+    name: string;
+    // Refere-se ao nome do cliente
+
+    registrationNumber: number;
+    // Refere-se ao número de cadastro do cliente na concessionária
+    // como se fosse um id
+
+    consumedEnergy: number;
+    // Refere-se à energia consumida pelo cliente no mês
+
+    calculateBill(): number;
+    // Retorna o valor da conta em reais
+}
+
+const client: Client = {
+    name: "Ronaldo",
+    registrationNumber: 2,
+    consumedEnergy: 150,
+
+    calculateBill: () => {
+        return 2
+    }
+}
+
+console.log(client.name, client.registrationNumber, client.consumedEnergy, client.calculateBill())
+
+/*
+    Exercício 1
+    a) Todas as propriedades foram impressas
+*/
+
+export abstract class Place {
+    constructor(protected cep: string) { }
+
+    public getCep(): string {
+        return this.cep;
+    }
+}
+
+// const place: Place = new Place()
+/*
+    Exercício 2
+    a) "Não pode criar uma instância de uma classe abstrata"
+    b) "Retirar o abstract e transformar em uma classe padrão"
+*/
+
+export class Residence extends Place {
+    constructor(
+        private dwellersQuantity: number,
+
+        cep: string
+    ) {
+        super(cep);
+    }
+
+    public getDwellersQuantity(): number {
+        return this.dwellersQuantity
+    }
+}
+
+export class Commerce extends Place {
+    constructor(
+        private floorsQuantity: number,
+        // Refere-se à quantidade de andares do lugar
+
+        cep: string
+    ) {
+        super(cep);
+    }
+
+    public getFloorsQuantity(): number {
+        return this.floorsQuantity;
+    }
+}
+
+export class Industry extends Place {
+    constructor(
+        private machinesQuantity: number,
+        // Refere-se à quantidade de máquinas do local
+
+        cep: string
+    ) {
+        super(cep);
+    }
+
+    public getMachinesQuantity(): number {
+        return this.machinesQuantity;
+    }
+}
+// Exercício 3
+const residence: Residence = new Residence(5, "45678-789")
+const commerce: Commerce = new Commerce(3, "45698-123")
+const industry: Industry = new Industry(35, "32145-456")
+
+console.log(residence.getCep(), commerce.getCep(), industry.getCep())
+
