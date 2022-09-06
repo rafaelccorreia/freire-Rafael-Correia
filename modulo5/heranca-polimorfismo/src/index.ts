31247204e7e10c55c3928a454ddf89b7b4580ad6
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-09-06 18:05:00 -0300

    Exercício 2,3,4 e 5

--- modulo5/heranca-polimorfismo/src/index.ts
+++ modulo5/heranca-polimorfismo/src/index.ts
@@ -28,6 +28,10 @@ class User {
     public getName(): string {
         return this.name
     }
+
+    public introduceYourserlf(): string {
+        return `Olá, sou ${this.name}. Bom dia!`
+    }
 }
 
 /*
@@ -39,3 +43,41 @@ class User {
 const user: User = new User("06", "carlos@carl.com", "Carlos", "senhaTeste")
 console.log(user.getId(), user.getName(), user.getEmail(), user.password)
 
+class Customer extends User {
+    public purchaseTotal: number = 0;
+    private creditCard: string;
+
+    constructor(
+        id: string,
+        email: string,
+        name: string,
+        password: string,
+        creditCard: string
+    ) {
+        super(id, email, name, password);
+        console.log("Chamando o construtor da classe Customer");
+        this.creditCard = creditCard;
+    }
+
+    public getCreditCard(): string {
+        return this.creditCard;
+    }
+}
+
+const customer: Customer = new Customer("1", "julia@email.com", "Julia", "senha123", "12345678998711")
+
+/*
+    Exercício 2
+    a) A mensagem aparece uma vez já que o constructor da classe Customer é chamado ao criar uma instância da classe
+    b) A mensagem aparece uma vez pois o constructor da classe User é chamado no "super" dentro do constructor da classe Customer
+*/
+
+console.log(customer.getId(), customer.getName(), customer.getEmail(), customer.purchaseTotal, customer.getCreditCard(), customer.password)
+
+/*
+    Exercício3
+    a) A senha aparece no console, porém não seria possível já que a senha está atrelada a classe User, e não foi criado nenhum método public para acessá-la
+*/
+
+// Exercício 4
+console.log(customer.introduceYourserlf())
